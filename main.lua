70f9d8e5ee1b4f172092f8d70e4141fcd49bbf43
k-rgz <98106637+k-rgz@users.noreply.github.com>
2022-05-16 22:25:15 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,81 +1,60 @@
---// https://discord.gg/ShY6aAu2y5 \\--
--- Gui to lua
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
 local ScreenGui = Instance.new("ScreenGui")
-local Fr = Instance.new("Frame")
-local counter = Instance.new("TextLabel")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
 local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
-ScreenGui.Parent = game.CoreGui
+local ImageButton = Instance.new("ImageButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-Fr.Name = "Fr"
-Fr.Parent = ScreenGui
-Fr.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Fr.Position = UDim2.new(0.0325704217, 0, 0.878603935, 0)
-Fr.Size = UDim2.new(0, 256, 0, 42)
-counter.Name = "counter"
-counter.Parent = Fr
-counter.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
-counter.Size = UDim2.new(0, 256, 0, 42)
-counter.Font = Enum.Font.SourceSans
-counter.Text = "KROP ON TOP"
-counter.TextColor3 = Color3.fromRGB(255, 255, 255)
-counter.TextSize = 14.000
-UICorner.Parent = counter
-UICorner_2.Parent = Fr
-local function IOGE_fake_script() -- counter.LocalScript 
-	local script = Instance.new('LocalScript', counter)
 
-workspace.Ignored.Drop.ChildAdded:Connect(function(child)
-if child:IsA("Part") then
-	local s = string.sub(child:WaitForChild("BillboardGui").TextLabel.Text, 2)
-	getgenv().Cash = getgenv().Cash + s
-end
-end)
-	
-	while wait(2) do
-		script.Parent.Text = "$"..getgenv().Cash
-	end
-end
-coroutine.wrap(IOGE_fake_script)()
-local function RMGQLT_fake_script() -- Fr.Dragify 
-	local script = Instance.new('LocalScript', Fr)
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+Frame.Position = UDim2.new(0.44921875, 0, 0.426850259, 0)
+Frame.Size = UDim2.new(0, 130, 0, 84)
 
-	local UIS = game:GetService("UserInputService")
-	function dragify(Frame)
-	    dragToggle = nil
-	    local dragSpeed = 0.50
-	    dragInput = nil
-	    dragStart = nil
-	    local dragPos = nil
-	    function updateInput(input)
-	        local Delta = input.Position - dragStart
-	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
-	    end
-	    Frame.InputBegan:Connect(function(input)
-	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
-	            dragToggle = true
-	            dragStart = input.Position
-	            startPos = Frame.Position
-	            input.Changed:Connect(function()
-	                if input.UserInputState == Enum.UserInputState.End then
-	                    dragToggle = false
-	                end
-	            end)
-	        end
-	    end)
-	    Frame.InputChanged:Connect(function(input)
-	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-	            dragInput = input
-	        end
-	    end)
-	    game:GetService("UserInputService").InputChanged:Connect(function(input)
-	        if input == dragInput and dragToggle then
-	            updateInput(input)
-	        end
-	    end)
-	end
-	
-	dragify(script.Parent)
-end
-coroutine.wrap(RMGQLT_fake_script)()
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 130, 0, 38)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = ".gg/DHS"
+TextLabel.TextColor3 = Color3.fromRGB(0, 255, 8)
+TextLabel.TextSize = 24.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+TextLabel_2.Position = UDim2.new(0.0384615399, 0, 0.5, 0)
+TextLabel_2.Size = UDim2.new(0, 120, 0, 36)
+TextLabel_2.Font = Enum.Font.SourceSansBold
+TextLabel_2.Text = "N/A"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+UICorner.CornerRadius = UDim.new(0, 4)
+UICorner.Parent = TextLabel_2
+
+UICorner_2.CornerRadius = UDim.new(0.0500000007, 0)
+UICorner_2.Parent = Frame
+
+ImageButton.Parent = Frame
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundTransparency = 1.000
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.869230747, 0, 0.15476191, 0)
+ImageButton.Rotation = 90.000
+ImageButton.Size = UDim2.new(0, 12, 0, 12)
+ImageButton.Visible = false
+ImageButton.Image = "rbxassetid://9649081774"
+ImageButton.ScaleType = Enum.ScaleType.Fit
